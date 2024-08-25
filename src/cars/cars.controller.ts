55cabe3f1b5544cc2046b7793516e0d7b79f1839
Ezzz-Lui <ogn.lui@gmail.com>
2024-08-24 22:28:30 -0600

    feat: Add getAllBrandsCars and getBrandCarById methods to CarsController
    - Implemented getAllBrandsCars method to retrieve all car brands.
    - Added getBrandCarById method with ParseIntPipe validation to retrieve a car brand by ID.

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -14,9 +14,11 @@ export class CarsController {
         return this.carsService.findAll()
     }
 
+
+    //Validacion con pipes que el id sea un numero
     @Get(':id')
-    getBrandCarById( @Param('id') id:string ){
-       return this.carsService.findOneById( +id )
+    getBrandCarById( @Param('id', ParseIntPipe ) id:number ){
+       return this.carsService.findOneById( id )
     }
 
 }
