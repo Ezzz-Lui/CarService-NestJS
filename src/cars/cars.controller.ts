194406609d7778b713a6c0edb07dafdce8ef4375
Ezzz-Lui <ogn.lui@gmail.com>
2024-08-24 23:04:15 -0600

    Build: Add new http methods as endpoints api

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { error } from 'console';
 
@@ -21,6 +21,29 @@ export class CarsController {
     getBrandCarById( @Param('id', ParseIntPipe ) id:number ){
 
        return this.carsService.findOneById( id )
+    }
+
+
+    //otros metodos HTPP: POST
+    @Post()
+    createCar( @Body() bodyRequest: any ){
+        return bodyRequest;
+    }
+
+    @Patch(':id')
+    updateCar( @Body() bodyUpdate: any ){
+        return bodyUpdate;
+    }
+
+
+    //delete method
+    @Delete(':id')
+    deleteCar( @Param('id', ParseIntPipe ) id: number){
+        return {
+            method: 'delete',
+            id
+        }
+
     }
 
 }
