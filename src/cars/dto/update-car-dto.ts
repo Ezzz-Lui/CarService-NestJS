960507c62fea772d11ad1ed5de457019e97bc0ff
Ezzz-Lui <ogn.lui@gmail.com>
2024-08-31 14:55:23 -0600

    feat: Add UpdateCarDTO for updating car information
    - Created UpdateCarDTO class to handle updating car information.
    - Added optional properties for id, brand, and model.

---
+++ src/cars/dto/update-car-dto.ts
@@ -0,0 +1,19 @@
+import { IsOptional, IsString, IsUUID } from 'class-validator'
+import { UUID } from 'crypto';
+export class CreatCarDTO {
+
+
+    @IsString()
+    @IsUUID()
+    @IsOptional() //Decorador para asignar que el parametro es opcional, si viene debe de ser uuid o string.
+    readonly id ?: UUID
+
+    @IsString()
+    @IsOptional()
+    readonly brand ?: string;
+
+    @IsString()
+    @IsOptional()
+    readonly model ?: string;
+
+}
