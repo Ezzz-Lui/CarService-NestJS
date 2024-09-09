380a608094f54dca2e24603c5bdfb41c7bc6099d
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-09 02:26:00 -0600

    feat: Add BrandsModule to app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CarsModule } from './cars/cars.module';
+import { BrandsModule } from './brands/brands.module';
 
 @Module({
-  imports: [CarsModule],
+  imports: [CarsModule, BrandsModule],
   controllers: [AppController],
   providers: [AppService],
 })
