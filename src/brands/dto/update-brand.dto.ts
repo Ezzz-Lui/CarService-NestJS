6cc87128700f720f44508217f72ca5f70561693d
Ezzz-Lui <ogn.lui@gmail.com>
2024-09-11 01:35:21 -0600

    refactor: Update UpdateBrandDto to include validation for the name field

--- src/brands/dto/update-brand.dto.ts
+++ src/brands/dto/update-brand.dto.ts
@@ -1,4 +1,12 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateBrandDto } from './create-brand.dto';
+// import { PartialType } from '@nestjs/mapped-types';
+// import { CreateBrandDto } from './create-brand.dto';
 
-export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+import { IsString, isString, MinLength } from "class-validator";
+
+// export class UpdateBrandDto extends PartialType(CreateBrandDto) {}
+export class UpdateBrandDto {
+
+    @IsString()
+    @MinLength(1)
+    name: string;
+}
